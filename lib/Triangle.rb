3299e2b7eb58a3e4a8cfe1f8ffbc254b70d295a5
Student <example@example.com>
2018-11-26 08:39:46 -0800

    add triangle function with passing tests

--- lib/Triangle.rb
+++ lib/Triangle.rb
@@ -1,5 +1,19 @@
 class Triangle
+  attr_accessor(:sides)
 
+  def initialize(side_a, side_b, side_c)
+    @sides = [side_a.to_i, side_b.to_i, side_c.to_i].sort!
+  end
+
+  def triangle?
+    if @sides[0] + @sides[1] <= @sides[2]
+      return false
+    elsif (@sides[0] == @sides[1]) && (@sides[1] == @sides[2])
+      return "equilateral"
+    elsif ((@sides[0] == @sides[1]) && (@sides[1] != @sides[2])) || ((@sides[0] != @sides[1]) && (@sides[1] == @sides[2]))
+      return "isosceles"
+    elsif (@sides[0] != @sides[1]) && (@sides[1] != @sides[2]) && (@sides[2] != @sides[1])
+      return "scalene"
+    end
+  end
 end
-
-
--- spec/ruby_spec.rb
+++ spec/ruby_spec.rb
@@ -1,17 +1,28 @@
 require ('rspec')
 require ('pry')
-require ('Triangle')
+require ('./lib/Triangle')
 
 
-describe('Triangle') do 
+describe('Triangle') do
 
-  it('tests a method for Triangle') do
+  it('test for equilateral triangle') do
+    dummy = Triangle.new(3,3,3)
+    expect(dummy.triangle?()).to(eq("equilateral"))
+  end
 
-    dummy = Triangle.new()
+  it('test for isosceles triangle') do
+    dummy = Triangle.new(3,3,1)
+    expect(dummy.triangle?()).to(eq("isosceles"))
+  end
 
-    expect(dummy.method()).to(eq(expected result))
+  it('test for scalene triangle') do
+    dummy = Triangle.new(3,4,5)
+    expect(dummy.triangle?()).to(eq("scalene"))
+  end
 
+  it('is a triangle') do
+    dummy = Triangle.new(1,3,5)
+    expect(dummy.triangle?()).to(eq(false))
   end
 
 end
-
